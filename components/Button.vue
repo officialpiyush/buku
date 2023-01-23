3750229f72b975826d224032951dd23ba2dba3f4
Piyush <bhangalepiyush@gmail.com>
2023-01-23 10:19:42 +0530

    feat(component): add Button component

---
+++ components/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button px-12 py-1 bg-green-600 bg-opacity-20 text-green-600 hover="bg-opacity-22" rounded>
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+</style>
