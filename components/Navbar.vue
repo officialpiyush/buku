874725564799ed4e083fe908a62ee9db3dc95e97
Piyush <bhangalepiyush@gmail.com>
2023-01-23 10:20:05 +0530

    feat(component): add navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div py-4 border-b-1 border-stone-600>
+    <div max-w-7xl mx-auto flex items-center justify-between>
+      <div flex items-center gap="1.5">
+        <div h-8 w-8 text-green-600 i-pixelarticons-book-open />
+        <div text-xl font-bold>
+          Buku
+        </div>
+      </div>
+
+      <div>
+        <Button px="12">
+          <div flex items-center gap-1>
+            <div h-4 w-4 i-ri-login-circle-line />
+            <span>Login</span>
+          </div>
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
