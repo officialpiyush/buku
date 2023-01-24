4e89487f038af7c308a44dd9eb7f843f8094d5fd
Piyush <bhangalepiyush@gmail.com>
2023-01-24 20:34:43 +0530

    fix: min-h-screen on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,11 @@
 <template>
-  <div bg-zinc-900>
-    <div font-mono lt-lg:min-h-screen lg:h-screen pb-4 text-white flex="~ col">
-      <nav flex-shrink-0>
-        <Navbar />
-      </nav>
-      <main flex-1 py-4 max-w-7xl mx-auto>
-        <slot />
-      </main>
-      <footer flex-shrink-0 />
-    </div>
+  <div font-mono min-h-screen lg:h-screen pb-4 bg-zinc-900 text-white flex="~ col">
+    <nav flex-shrink-0>
+      <Navbar />
+    </nav>
+    <main flex-1 py-4 max-w-7xl mx-auto>
+      <slot />
+    </main>
+    <footer flex-shrink-0 />
   </div>
 </template>
