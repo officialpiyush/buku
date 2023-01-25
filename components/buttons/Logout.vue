1679708aa7e78c12595ef56572f440a466ba9c01
Piyush <bhangalepiyush@gmail.com>
2023-01-25 21:39:25 +0530

    feat: add logout button

---
+++ components/buttons/Logout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { signOut as firebaseSignOut } from 'firebase/auth'
+const auth = useFirebaseAuth()!
+
+const signOut = () => firebaseSignOut(auth)
+</script>
+
+<template>
+  <button flex items-center justify-center gap-1 px-8 py-2 bg-red-600 bg-opacity-20 text-red-600 hover="bg-opacity-22" rounded @click="signOut">
+    <div h-4 w-4 i-ri-google-fill />
+    <span>Logout</span>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
