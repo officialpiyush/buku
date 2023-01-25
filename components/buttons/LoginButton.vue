e837f6653d11c1d3f252d2d101c59d02c282662f
Piyush <bhangalepiyush@gmail.com>
2023-01-25 21:33:59 +0530

    feat: center horizontally the button

--- components/buttons/LoginButton.vue
+++ components/buttons/LoginButton.vue
@@ -9,7 +9,7 @@ const signIn = async () => {
 </script>
 
 <template>
-  <button flex items-center gap-1 px-8 py-1 bg-green-600 bg-opacity-20 text-green-600 hover="bg-opacity-22" rounded @click="signIn">
+  <button flex items-center justify-center gap-1 px-8 py-2 bg-green-600 bg-opacity-20 text-green-600 hover="bg-opacity-22" rounded @click="signIn">
     <div h-4 w-4 i-ri-google-fill />
     <span>Login</span>
   </button>
