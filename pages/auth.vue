1573fc8425646939bc03531c3dd5e24259047ed9
Piyush <bhangalepiyush@gmail.com>
2023-01-25 21:39:50 +0530

    feat: add auth page

---
+++ pages/auth.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const user = useCurrentUser()
+</script>
+
+<template>
+  <div>
+    <div v-if="!user" flex flex-col gap-4>
+      You need to be logged in to access the page
+      <ButtonsLoginButton />
+    </div>
+
+    <div v-else flex flex-col gap-4>
+      You are logged in as {{ user.displayName }}
+      <ButtonsLogout />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
