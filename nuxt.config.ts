56e3a25a62638ec1b1a14d8cc6d6665bb1865bee
Piyush <bhangalepiyush@gmail.com>
2023-01-26 14:14:45 +0530

    chore: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import destr from 'destr'
+import type { VueFireNuxtModuleOptions } from 'nuxt-vuefire'
 import type { BuildInfo } from './types'
 
 export default defineNuxtConfig({
@@ -9,27 +10,28 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@nuxtjs/color-mode',
     '~/modules/build-env',
-    'nuxt-vuefire',
+    [
+      'nuxt-vuefire',
+      {
+        auth: true,
+        config: {
+          apiKey: 'AIzaSyBjHeKo26xCG1B2UG03pZQYp9sIz9pP7Uk',
+          authDomain: 'buku-v2.firebaseapp.com',
+          projectId: 'buku-v2',
+          storageBucket: 'buku-v2.appspot.com',
+          messagingSenderId: '630053346756',
+          appId: '1:630053346756:web:8e630c742a612a4f5a5891',
+          measurementId: 'G-LCCND887N7',
+        },
+        admin: {
+          serviceAccount: destr(process.env.FIREBASE_SERVICE_ACCOUNT as string),
+        },
+      } as VueFireNuxtModuleOptions],
   ],
   runtimeConfig: {
     public: {
       env: '',
       buildInfo: {} as BuildInfo,
-    },
-  },
-  vuefire: {
-    auth: true,
-    config: {
-      apiKey: 'AIzaSyBjHeKo26xCG1B2UG03pZQYp9sIz9pP7Uk',
-      authDomain: 'buku-v2.firebaseapp.com',
-      projectId: 'buku-v2',
-      storageBucket: 'buku-v2.appspot.com',
-      messagingSenderId: '630053346756',
-      appId: '1:630053346756:web:8e630c742a612a4f5a5891',
-      measurementId: 'G-LCCND887N7',
-    },
-    admin: {
-      serviceAccount: destr(process.env.FIREBASE_SERVICE_ACCOUNT as string),
     },
   },
   experimental: {
