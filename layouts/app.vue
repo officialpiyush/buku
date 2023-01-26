528534be285b86257f843adecb9d23a638d511c2
Piyush <bhangalepiyush@gmail.com>
2023-01-26 15:59:58 +0530

    feat(layout/app): add the layout used in the main app

---
+++ layouts/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div bg-zinc-900>
+    <div font-mono min-h-screen pb-4 text-white flex="~ col">
+      <nav flex-shrink-0>
+        <Navbar />
+      </nav>
+      <main w-full flex-1 p-4 max-w-7xl mx-auto gap-4 grid="~ cols-1 lg:cols-12">
+        <div lg:col-span-2 />
+        <div lg:col-span-8>
+          <slot />
+        </div>
+        <div lg:col-span-2 />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  </div>
+</template>
