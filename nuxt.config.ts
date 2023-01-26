8c27ddc00bd4dd92074dffdb9d1cec7137c9aef5
Piyush <bhangalepiyush@gmail.com>
2023-01-26 20:31:13 +0530

    build: try using all values

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,16 @@ import type { VueFireNuxtModuleOptions } from 'nuxt-vuefire'
 import type { BuildInfo } from './types'
 
 const customConfig = {
+  type: 'service_account',
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY!.replaceAll(/\\n/gm, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
 }
 
 export default defineNuxtConfig({
