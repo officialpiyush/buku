06285dd0610397dc253bf1e230d2a3072737f361
Piyush <bhangalepiyush@gmail.com>
2023-01-26 20:34:09 +0530

    ci: try delcaring ms globally

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import type { VueFireNuxtModuleOptions } from 'nuxt-vuefire'
 import type { BuildInfo } from './types'
 
+// @ts-expect-error didn't declare typings, testing
+globalThis.ms = require(
+  'ms',
+)
+
 const customConfig = {
   type: 'service_account',
   project_id: process.env.FIREBASE_PROJECT_ID,
