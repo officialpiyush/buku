e8ae4046c1c66558c46f9decd0f4b49733de9d7f
Piyush <bhangalepiyush@gmail.com>
2023-01-27 16:17:17 +0530

    feat: add demo cards to app

--- pages/app.vue
+++ pages/app.vue
@@ -3,11 +3,20 @@ definePageMeta({
   layout: 'app',
   middleware: ['auth'],
 })
+
+const dummyArray = new Array(20).fill(0).map((_, i) => i)
 </script>
 
 <template>
   <div>
-    // TODO
+    <div relative flex="~ col" gap-4>
+      <div sticky top-0 lrft-0 class="text-2xl font-bold" backdrop="~ blur-lg" py-2 px-2>
+        Starred
+      </div>
+      <div grid="~ cols-1 sm:cols-2 lg:cols-4" gap-4>
+        <BookInfoCard v-for="i in dummyArray" :key="i" :number="i" />
+      </div>
+    </div>
   </div>
 </template>
 
