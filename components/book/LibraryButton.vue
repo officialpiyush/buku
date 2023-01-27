e8c8138e6bf374ddaa0c802336392f13593a8436
Piyush <bhangalepiyush@gmail.com>
2023-01-27 16:15:56 +0530

    feat(components): add dummy buttons for bookinfocard

---
+++ components/book/LibraryButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <button text-sm text-center py-2 px-4 rounded border border-zinc-700 hover:bg-zinc-800>
+    Library
+  </button>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/book/OpenButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <button text-sm text-center py-2 px-4 rounded bg-green-700 bg-opacity-40>
+    Open
+  </button>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
