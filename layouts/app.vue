253fbbf62f912e83a23f6421766e4dd09221114c
Piyush <bhangalepiyush@gmail.com>
2023-01-27 15:53:42 +0530

    feat(layout/app): more column space to books, move user card to right

--- layouts/app.vue
+++ layouts/app.vue
@@ -1,17 +1,15 @@
 <template>
   <div bg-zinc-900>
-    <div font-mono min-h-screen pb-4 text-white flex="~ col">
+    <div font-mono lt-lg:min-h-screen lg:h-screen pb-12 lg:overflow-y-hidden text-white flex="~ col">
       <nav flex-shrink-0>
         <Navbar />
       </nav>
-      <main w-full flex-1 p-4 max-w-7xl mx-auto gap-4 grid="~ cols-1 lg:cols-12">
-        <div lg:col-span-2>
-          <UserCard />
-        </div>
-        <div lg:col-span-8>
+      <main pb-22 flex-1 h-screen lg:max-h-full w-full p-4 max-w-7xl mx-auto gap-4 grid="~ cols-1 lg:cols-12">
+        <div overflow="auto" lg:col-span-10 h-full>
           <slot />
         </div>
-        <div lg:col-span-2 flex flex-col justify="end" items-center>
+        <div lg:col-span-2 flex flex-col justify="between" items-center gap-4>
+          <UserCard />
           <Footer :break="true" />
         </div>
       </main>
