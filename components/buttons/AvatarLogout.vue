6b1d4471e232aed45f86a0a51f6ac4ddadd94c00
Piyush <bhangalepiyush@gmail.com>
2023-01-27 14:42:29 +0530

    feat: reroute to home after logout

--- components/buttons/AvatarLogout.vue
+++ components/buttons/AvatarLogout.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { signOut as firebaseSignOut } from 'firebase/auth'
+const router = useRouter()
 const auth = useFirebaseAuth()!
 const user = useCurrentUser()
 
-const userName = computed(() => user.value?.displayName?.split(' ')[0] ?? 'User')
-
-const signOut = () => firebaseSignOut(auth)
+const signOut = async () => {
+  await firebaseSignOut(auth)
+  await router.push('/')
+}
 
 function useKittenImage(imageErrorEvent: Event) {
   (imageErrorEvent.target as HTMLImageElement).src = 'https://placekitten.com/96/140'
@@ -14,7 +16,10 @@ function useKittenImage(imageErrorEvent: Event) {
 
 <template>
   <button rounded-full @click="signOut">
-    <img h-8 w-8 rounded-full :src="user?.photoURL || 'https://placekitten.com/96/140'" alt="dp" @error="useKittenImage">
+    <img
+      h-8 w-8 rounded-full :src="user?.photoURL || 'https://placekitten.com/96/140'" alt="dp"
+      @error="useKittenImage"
+    >
   </button>
 </template>
 
--- components/buttons/Logout.vue
+++ components/buttons/Logout.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { signOut as firebaseSignOut } from 'firebase/auth'
 const auth = useFirebaseAuth()!
+const router = useRouter()
 
-const signOut = () => firebaseSignOut(auth)
+const signOut = async () => {
+  await firebaseSignOut(auth)
+  await router.push('/')
+}
 </script>
 
 <template>
-  <button flex items-center justify-center gap-1 px-8 py-2 bg-red-600 bg-opacity-20 text-red-600 hover="bg-opacity-22" rounded @click="signOut">
+  <button
+    flex items-center justify-center gap-1 px-8 py-2 bg-red-600 bg-opacity-20 text-red-600 hover="bg-opacity-22"
+    rounded @click="signOut"
+  >
     <div h-4 w-4 i-ri-google-fill />
     <span>Logout</span>
   </button>
